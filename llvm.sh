b8b3ca4a7c5d27536f6c95d825902bcfcc62631c
ycsheu <ycsheu@pllab.cs.nthu.edu.tw>
2018-04-25 17:10:52 +0800

    add script

---
+++ llvm.sh
@@ -0,0 +1,44 @@
+set -eux
+version="$1"
+THREADS=1
+CMAKE=cmake
+INSTALL_PATH=${HOME}/compilers/llvm-${version}
+TARGETS="X86"
+#if test -f gcc-"$version".tar.xz
+#then
+#echo 'already exist gcc-"$version".tar.xz'
+#else
+mkdir -p llvm-${version}
+cd llvm-${version}
+mkdir -p source_code
+cd source_code
+if test -f llvm-${version}.src
+then
+echo 'already exist llvm source code'
+else
+wget -c http://releases.llvm.org/${version}/llvm-${version}.src.tar.xz
+tar -xf llvm-${version}.src.tar.xz
+fi
+if test -f llvm-${version}.src/tools/clang
+then
+echo 'already exist clang'
+else
+wget -c http://releases.llvm.org/${version}/cfe-${version}.src.tar.xz
+tar -xf cfe-${version}.src.tar.xz
+mv cfe-${version}.src llvm-${version}.src/tools/clang
+fi
+if test -f llvm-${version}.src/projects/compiler-rt
+then
+echo 'already exist compiler-rt'
+else
+wget -c http://releases.llvm.org/${version}/compiler-rt-${version}.src.tar.xz
+tar -xf compiler-rt-${version}.src.tar.xz
+mv compiler-rt-${version}.src llvm-${version}.src/projects/compiler-rt
+fi
+cd ..
+mkdir -p build
+cd build
+${CMAKE} ../source_code/llvm-${version}.src -DCMAKE_INSTALL_PREFIX=$INSTALL_PATH} -DCMAKE_BUILD_TYPE=Debug -DLLVM_ENABLE_ASSERTIONS=ON -DLLVM_TARGETS_TO_BUILD=${TARGETS} -DLLVM_BUILD_TESTS=ON -DLLVM_BUILD_EXAMPLES=ON
+make -j${THREADS}
+make check-all
+make install
