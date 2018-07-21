675d6be25733467b57e83adb17633c22343fdeff
ycsheu <38524722+ycsheu@users.noreply.github.com>
2018-07-21 14:53:54 +0800

    Update llvm.sh

--- llvm.sh
+++ llvm.sh
@@ -4,6 +4,8 @@ THREADS=1
 CMAKE=cmake
 INSTALL_PATH=${HOME}/compilers/llvm-${version}
 TARGETS="X86"
+COMP_FORMAT=xz
+CLANG_NAME=cfe
 #if test -f gcc-"$version".tar.xz
 #then
 #echo 'already exist gcc-"$version".tar.xz'
@@ -16,29 +18,29 @@ if test -f llvm-${version}.src
 then
 echo 'already exist llvm source code'
 else
-wget -c http://releases.llvm.org/${version}/llvm-${version}.src.tar.xz
-tar -xf llvm-${version}.src.tar.xz
+wget -c http://releases.llvm.org/${version}/llvm-${version}.src.tar.${COMP_FORMAT}
+tar -xf llvm-${version}.src.tar${COMP_FORMAT}
 fi
 if test -f llvm-${version}.src/tools/clang
 then
 echo 'already exist clang'
 else
-wget -c http://releases.llvm.org/${version}/cfe-${version}.src.tar.xz
-tar -xf cfe-${version}.src.tar.xz
-mv cfe-${version}.src llvm-${version}.src/tools/clang
+wget -c http://releases.llvm.org/${version}/${CLANG_NAME}-${version}.src.tar${COMP_FORMAT}
+tar -xf ${CLANG_NAME}-${version}.src.tar${COMP_FORMAT}
+mv ${CLANG_NAME}-${version}.src llvm-${version}.src/tools/clang
 fi
 if test -f llvm-${version}.src/projects/compiler-rt
 then
 echo 'already exist compiler-rt'
 else
-wget -c http://releases.llvm.org/${version}/compiler-rt-${version}.src.tar.xz
-tar -xf compiler-rt-${version}.src.tar.xz
+wget -c http://releases.llvm.org/${version}/compiler-rt-${version}.src.tar${COMP_FORMAT}
+tar -xf compiler-rt-${version}.src.tar${COMP_FORMAT}
 mv compiler-rt-${version}.src llvm-${version}.src/projects/compiler-rt
 fi
 cd ..
 mkdir -p build
 cd build
-${CMAKE} ../source_code/llvm-${version}.src -DCMAKE_INSTALL_PREFIX=$INSTALL_PATH} -DCMAKE_BUILD_TYPE=Debug -DLLVM_ENABLE_ASSERTIONS=ON -DLLVM_TARGETS_TO_BUILD=${TARGETS} -DLLVM_BUILD_TESTS=ON -DLLVM_BUILD_EXAMPLES=ON
+${CMAKE} ../source_code/llvm-${version}.src -DCMAKE_INSTALL_PREFIX=${INSTALL_PATH} -DCMAKE_BUILD_TYPE=Debug -DLLVM_ENABLE_ASSERTIONS=ON -DLLVM_TARGETS_TO_BUILD=${TARGETS} -DLLVM_BUILD_TESTS=ON -DLLVM_BUILD_EXAMPLES=ON
 make -j${THREADS}
 make check-all
 make install
